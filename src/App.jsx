cc7681ef23d49280c639fed84887d8e58ec46391
BeckyBox <unsworthbecky@gmail.com>
2023-12-11 13:17:00 +0000

    added figma plan

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import { useState } from 'react'
 import './App.css'
-
+import Header from './components/header'
+import Navbar from './components/navBar'
 function App() {
-
-  
+  return (
+<div>
+      <Header/>
+      <Navbar/>
+</div>
+  )
 }
 
 export default App
---
+++ src/components/navBar.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom"
+
+const Navbar = () => {
+    return ( 
+        <nav>
+            <Link>Home </Link>
+            <Link> users</Link>
+            
+        </nav> )
+}
+
+
+export default Navbar
