4ba341c34346ffe6ed1daf1c7235871d50b87ef6
BeckyBox <unsworthbecky@gmail.com>
2023-12-14 16:59:21 +0000

    started on topics pages

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,12 +1,31 @@
 import { Link } from "react-router-dom"
+import { getAllTopics } from "../utils"
+import { useState, useEffect } from "react"
 
 const Navbar = () => {
+    const [topics, setTopics] = useState([])
+    useEffect(() => {
+        getAllTopics().then(({topics}) => {
+          
+            setTopics(topics)
+        });
+    }, [])
+ 
+
     return ( 
-        <nav>
+        <div><nav>
             <Link to={"/"} className="nav-text">Home</Link>
-            <Link className="nav-text">Users</Link> 
+            <Link className="nav-text">Users</Link>
+            <Link className="nav-text-user">User : weegembum</Link> 
             
-        </nav> )
+        </nav>
+        <nav>
+            {topics.map((topic) => {
+                return <Link to={`/articles/${topic.slug}`}key={topic.slug} className="topic-link">{topic.slug}</Link>
+            }) }
+            
+        </nav>
+        </div> )
 }
 
 
