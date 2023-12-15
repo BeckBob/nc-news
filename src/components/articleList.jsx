0f9dd269da2cb15e4f45b7146fe60275af7151c4
BeckyBox <unsworthbecky@gmail.com>
2023-12-15 12:32:50 +0000

    added option to order articles by categories

--- src/components/articleList.jsx
+++ src/components/articleList.jsx
@@ -6,13 +6,38 @@ import ArticleCard from "./articleCard";
 const ArticleList = (props) => {
     const [articles, setArticles] = useState([])
     const [isLoading, setIsLoading] = useState(false) 
+    const topics = ["date", "comments", "votes"]
+    const [sortBy, setSortBy] = useState("date")
+    const [orderBy, setOrderBy] = useState("desc")
+
+
     useEffect(() => {
         setIsLoading(true)
 		getAllArticles().then(({body}) => {
-			setArticles(body);
-            setIsLoading(false)
+            const sortArray = (type, order) => {
+                const types = {
+                  date: 'created_at',
+                  comments: 'comment_count',
+                  votes: 'votes',
+                };
+                
+                const sortProperty = types[type];
+                let sorted = [...body].sort((a, b) => b[sortProperty] - a[sortProperty])
+                
+                if(order === "asc"){
+                sorted = [...body].sort((a, b) => a[sortProperty] - b[sortProperty]);}
+                setArticles(sorted);
+                setIsLoading(false)
+              };
+              console.log(articles)
+              sortArray(sortBy, orderBy);
+			
+            
 		});
-    }, [])
+    }, [sortBy, orderBy])
+    
+ 
+    
 
     if (isLoading){
         return <section className="loading-screen">loading...</section>
@@ -28,6 +53,11 @@ const ArticleList = (props) => {
                 </section>)}
       else
 return (<section className="articles-container">
+            <div><form><label htmlFor="sortby-category">sort by<select id="sortyby-category" name="sortby-category"> 
+            {topics.map((topic) => { return <option key={topic} value={topic} onClick={(e) => setSortBy(e.target.value)}>{topic}</option>})}
+                 </select></label><label htmlFor="orderby">order by<select id="orderby" name="orderby">
+                    <option key={"desc"} value={"desc"} onClick={(e) => setOrderBy(e.target.value)}>desc</option><option key={"asc"} value={"asc"} onClick={(e) => setOrderBy(e.target.value)}>asc</option>
+        </select></label></form></div>
 {articles.map((article) => {
     return (
         <Link to={`/articles/${article.article_id}`} key={article.article_id} className="article-link">
